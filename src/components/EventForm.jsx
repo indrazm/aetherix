738718eb5fed2b7bfa1f854733a0e21e2b00bb34
toer <masturt22@gmail.com>
2023-09-20 22:38:09 +0700

    hapus div line 5 dan 7

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 export const EventForm = () => {
   return (
-    <div className="bg-white/50 p-8 rounded-lg border-2 shadow text-slate-800 mt-8">
-      <div className="text-2xl font-bold text-center mb-4">New Event</div>
-      <div className="textEvent">
-        <div>Event Title</div>
+    <div className="bg-slate-200/50 p-8 rounded-lg border-2 shadow text-slate-800 mt-8">
+      <h1 className=" text-2xl font-bold text-center mb-4">New Event</h1>
+
+      <div className="font-bold textEvent">
+        Event Title
         <input />
       </div>
+
       <div className="textEvent">
-        <div>Event Description</div>
+        Event Description
         <input />
       </div>
+
       <div className="textEvent">
-        <div> Event Date</div>
+        Event Date
         <input type="date" />
       </div>
+
       <div className="text-center">
-        <button className="">Create Event</button>
+        <button>Create Event</button>
       </div>
     </div>
   );
