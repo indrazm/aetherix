9d38b0babca1a029b507f8d5835541cba7d4e564
Agus Muhammad <agusmu7@gmail.com>
2023-09-24 07:51:37 +0700

    Create Dashboard Page for User with All Events Card #4

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 
-import { Login } from "@/components/Auth/Login"
+import { Header } from "@/components/Header";
+import { EventCard } from "@/components/EventCard";
 
 export default function Home() {
   return (
-    <div className= flex flex-col items-center justify-center	h-screen>
-      <div classNAme= w-300 mt-20>
-        <Login />
+    <main className="flex flex-col min-h-screen">
+      <Header />
+      <div className="max-w-screen-xl flex flex-col mx-auto px-4 py-8">
+        <div className="w-full flex flex-wrap items-center justify-between mx-auto mb-8">
+          <h1 className="text-3xl font-bold">All Events</h1>
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">Add New Event</button>
+        </div>
+
+        <EventCard />
       </div>
-    </div>
-  )
+    </main>
+  );
+ }
---
+++ src/components/EventCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const EventCard = () => {
+  return (
+
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 001</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 002</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 003</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 004</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 005</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+        <div className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded-lg">
+            <div className="max-w-2xl mx-auto text-gray-500 space-y-4">
+                <h3 className="text-lg font-semibold text-gray-900">Event Title 006</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <span className="block text-sm">29 September 2023</span>
+            </div>
+        </div>
+    </div>
+
+  );
+};
